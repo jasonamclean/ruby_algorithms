9a74f4ed4172a706ce469210e1c7cfe84d30f548
Jason McLean <jasonamclean@hotmail.com>
2016-11-28 10:28:48 -0700

    fibonacci even

---
+++ fibonacci_even.rb
@@ -0,0 +1,27 @@
+# By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
+
+class Fibbing
+  def initialize(max)
+    @num_1 = 0
+    @i = 0
+    @sum = 0
+    @num_2 = 1
+    @max = max
+  end
+
+  def even_fibonacci
+    while @i <= @max
+      @i = @num_1 + @num_2
+      @sum += @i if @i % 2 == 0
+      @num_1 = @num_2
+      @num_2 = @i
+    end
+
+    @sum
+  end
+end
+
+result = Fibbing.new(4_000_000)
+p result.even_fibonacci
+
+# solution for 4,000,000 is: 4,613,732
