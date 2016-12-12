477261352cc20315781817ba628c514d060a3d5d
Jason McLean <jasonamclean@hotmail.com>
2016-12-12 09:58:24 -0700

    permutation algorithm

---
+++ permutation_algorithm.rb
@@ -0,0 +1,13 @@
+# What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ?
+
+p [0,1,2,3,4,5,6,7,8,9].permutation.to_a[999_999].join
+# Solution: 2,783,915,460
+
+
+# Permutation example:
+# [1, 2, 3]
+# 123
+# 132
+# 213
+# 231
+# 231...
